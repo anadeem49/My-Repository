e54746435841098c62bce3a6993647d5fa67b3d7
anadeem49 <41883151+anadeem49@users.noreply.github.com>
2019-01-24 16:31:41 -0500

    Update passwordchecker.java

--- passwordchecker.java
+++ passwordchecker.java
@@ -16,6 +16,66 @@ public class passwordCheck{
       
       password = infile.nextLine();
       
-      if(testPassword
-
+      if(testPassword(password)){
+        
+        System.out.println("You entered " + password);
+        
+        System.out.println("Password is valid");
+        
+      }
+      
+      else{
+        
+        System.out.pritnln("You entered " + password);
+        
+        System.out.println("Password is invalid");
+        
+      }
+    
+    }
+    
+    System.out.println("The program is finished");
+    
+    infile.close();
+    
+  }
   
+//I: String password
+//P: Test its length, whether it hs upper/lower case letters and digits
+//O: Return a boolean called valid to the main
+  
+  public static boolean testPassword(String password) throws IOExeception{
+    
+    boolean hasUpperCase = password.equals(password.toUpperCase());
+    
+    boolean hasLowerCase = password.equals(password.toLowerCase());
+    
+    boolean valid = false;
+    
+    if (password.length()) >= 8){
+      
+      valid = true;
+      
+    if (password.matches("[0 - 9]"))
+      
+      valid = true;
+      
+    if (password.indexOf(" ") == -1)
+      
+      valid = true;
+      
+    if (hasUpperCase && hasLowerCase)
+      
+      valid = true;
+      
+    else
+      
+      valid = false;
+      
+    }
+    
+    return valid;
+    
+  }
+  
+}
