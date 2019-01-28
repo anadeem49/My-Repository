984c0a50d65df01936010625806e07a7acc37a30
anadeem49 <41883151+anadeem49@users.noreply.github.com>
2019-01-27 20:00:46 -0500

    Update charityCollector.java

--- charityCollector.java
+++ charityCollector.java
@@ -6,8 +6,21 @@ import java.util.Scanner;
 import java.io.*;
 
 public class charityCollector{
+  
   public static void main(String[] args) throws IOException{
     
-    File file
+    File file = new File("input.txt");
+    
+    Scanner input = new Scanner(file);
+    
+    int[] idNumbers = new int [50];
+    
+    double[] donations = new double [50];
+    
+    double median1 = 0, total = 0;
+    
+    int donorCount = readArray(idNumbers, donations, input);
+    
+    //return 
     
     
