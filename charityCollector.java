d265a37f96c0725f52f611ad87bdb5650304d40a
anadeem49 <41883151+anadeem49@users.noreply.github.com>
2019-01-27 19:48:31 -0500

    Update charityCollector.java

--- charityCollector.java
+++ charityCollector.java
@@ -1,1 +1,13 @@
+/*This program inputs an array of idNumbers amd donations
+ * and sorts them via selectionSort and bubbleSort, also returning
+ * the total donors, total amount donated, the highest and lowest donations.*/
 
+import java.util.Scanner;
+import java.io.*;
+
+public class charityCollector{
+  public static void main(String[] args) throws IOException{
+    
+    File file
+    
+    
