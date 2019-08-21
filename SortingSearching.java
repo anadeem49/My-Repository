65b68617741b025270afcd9bd3136d450bd90d8a
Ahsan Nadeem <41883151+anadeem49@users.noreply.github.com>
2019-08-20 22:36:59 -0400

    Update SortingSearching.java

--- SortingSearching.java
+++ SortingSearching.java
@@ -10,7 +10,7 @@ public class SortingSearching{
 	public static int [] readFile(String fileName, String heading, Scanner readF) {
 		
 		array = null; int counter = 0;
-		 readF = null;
+		readF = null;
 
 		try{
 			readF = new Scanner(new File(fileName));
@@ -53,32 +53,32 @@ public class SortingSearching{
 	}
 	
 	public static int [] quickSort(int [] array, int low, int high) {
-		if (low < high){ 
-            int pi = partition(array, low, high); 
-            quickSort(array, low, pi-1); 
-            quickSort(array, pi+1, high); 
-        }
+		if (low < high){
+			int pi = partition(array, low, high); 
+            		quickSort(array, low, pi-1); 
+            		quickSort(array, pi+1, high); 
+        	}
 		return array;
 	}
 	
 	public static int partition(int array[], int low, int high){ 
-        int pivot = array[high];  
-        int i = low - 1; 
-        for (int j=low; j<high; j++){ 
-            if (array[j] <= pivot){ 
-                i++;
-                int temp = array[i]; 
-                array[i] = array[j]; 
-                array[j] = temp;
-            }
-            quickCompare++;
-        } 
-        int temp = array[i+1]; 
-        array[i+1] = array[high]; 
-        array[high] = temp; 
-        quickSwap++;
-        return i+1; 
-    }
+        	int pivot = array[high];  
+        	int i = low - 1; 
+        	for (int j=low; j<high; j++){ 
+            		if (array[j] <= pivot){ 
+                		i++;
+                		int temp = array[i]; 
+                		array[i] = array[j]; 
+                		array[j] = temp;
+            		}
+            		quickCompare++;
+        	}
+        	int temp = array[i+1]; 
+        	array[i+1] = array[high]; 
+        	array[high] = temp; 
+        	quickSwap++;
+        	return i+1; 
+    	}
 	
 	public static int [] selectionSort(int [] array) {
 		selectionCompare = selectionSwap = 0;
@@ -89,10 +89,10 @@ public class SortingSearching{
 					min = j;
 			selectionCompare++;
 			int temp = array[min]; 
-	        array[min] = array[i]; 
-	        array[i] = temp;
-	        selectionSwap++;
-	    }
+	        	array[min] = array[i]; 
+	        	array[i] = temp;
+	        	selectionSwap++;
+	    	}
 		return array;
 	}
 	
