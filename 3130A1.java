3543f34505a1940183f54023f62e2a6331b278e5
Ahsan Nadeem <41883151+anadeem49@users.noreply.github.com>
2020-02-18 18:52:43 -0500

    Update 3130A1.java

--- 3130A1.java
+++ 3130A1.java
@@ -1,5 +1,6 @@
 import java.io.*;
 import java.util.*;
+//Import statements for both classes
 
 public class StreamingArtists {
 	
@@ -7,7 +8,24 @@ public class StreamingArtists {
 	 * PROCESS: It inputs the data from the CSV files into the 2d array
 	 * OUTPUT: Doesn't return any output, but calls printArray to print out the 2d array
 	 * */
-	public static void input(Scanner file, int rows, int columns, String[][] songs){
+	String[][] songs;
+	
+	public static String[][] readFileAndGetSongs() {
+		Scanner file;
+		try {
+			file = new Scanner(new File("names.txt"));
+		    int rows = 8, columns = 3;
+		    String[][] songs = new String[rows][columns];
+		    songs = input(file, rows, columns, songs);
+		    return songs;
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}   
+		
+		return new String[0][0];
+	}
+	public static String[][] input(Scanner file, int rows, int columns, String[][] songs){
 		songs = new String [rows][columns];
 		for (int i = 0; i < rows; i++) {
 			String split [] = file.nextLine().split(",");
@@ -15,20 +33,41 @@ public class StreamingArtists {
 				songs[i][j] = split[j];
 			}
 		}
-		printArray(songs, rows, columns);
+		return sortArray(songs);
 	}
 	
 	/**INPUT: This method takes in the 2d String called songs and its rows and columns
 	 * PROCESS: This method prints out the 2d array
 	 * OUTPUT: This method doesn't return an output
 	 * */
-	public static void printArray(String [][] songs, int rows, int columns) {
-		for(int i = 0; i < rows; i++) {
-			for(int j = 0; j < columns; j++) {
+	public static void printArray(String [][] songs) {
+		for(int i = 0; i < songs.length; i++) {
+			for(int j = 0; j < songs[i].length; j++) {
 				System.out.print(songs[i][j]);
 			}
 			System.out.println();
 		}
+	}
+	
+	/**INPUT: This method takes in the 2d Songs array
+	 * PROCESS: This method uses bubble sort to sort by artist name
+	 * OUTPUT: This outputs the sorted array
+	 * */
+	public static String[][] sortArray(String [] [] songs) {
+		for (int i = 0; i < songs.length; i++) {
+			for (int j = i + 1; j < songs.length; j++) {
+				String[] curr = songs[i];
+				String[] next = songs[j];
+				String currArtistName = curr[1].split("by ")[1];
+				String nextArtistName = next[1].split("by ")[1];
+				if (currArtistName.compareTo(nextArtistName) > 0) {
+					songs[j] = curr;
+					songs[i] = next;
+				}
+			}
+		}
+		printArray(songs);
+		return songs;
 	}
 	
 	/**INPUT: The main method always takes in a String array called "args"
@@ -37,7 +76,7 @@ public class StreamingArtists {
 	 * */
 	public static void main(String[] args) throws Exception{
 		Scanner file = new Scanner(new File("names.txt"));
-	    int rows = 5, columns = 3;
+	    int rows = 8, columns = 3;
 	    String[][] songs = new String[rows][columns];
 	    input(file, rows, columns, songs);
 	}
@@ -52,36 +91,21 @@ class Node{
 	Node(String data) {this.data = data;}
 }
 
-public class TopStreamingArtists extends StreamingArtists{
+public class TopStreamingArtists {
 	
-	private static String[] songs = new String [15];	
 	private static LinkedList <String> ordered = new LinkedList<String>();
-	private static String data; private static Node next;
 	
 	/**INPUT: This method takes in a Scanner object called "text"
 	 * PROCESS: It inserts the data into the LinkedList from the CSV in ascending order
 	 * OUTPUT: It returns a fully loaded LinkedList containing String data
 	 * */
-	public static LinkedList<String> insert (Scanner text) {
-		while(text.hasNextLine()) { 
-			data = text.nextLine();
-			String [] split = data.split(",");
-			int n = ordered.size();
-			if (n == 0) {
-				ordered.add(data);
-			} else if (ordered.get(0).compareTo(split[1]) > 0) {
-				ordered.add(0, data);
-			} else if (ordered.get(n - 1).compareTo(split[1]) < 0) {
-				ordered.add(n, data);
-			} else {
-				int i = 0;
-				while (ordered.get(i).compareTo(split[1]) < 0) {
-					i++;
-				}
-				ordered.add(i, data);
+	public static LinkedList<String> insert (String [][] songs) {
+		for(int i = 0; i < songs.length; i++) {
+			for (int j = 0; j < songs[i].length; j++) {
+				ordered.add(songs[i][j]);
 			}
-		}	
-		System.out.print(ordered);
+		}
+		System.out.println(ordered);
 		return ordered;
 	}
 	
@@ -89,21 +113,10 @@ public class TopStreamingArtists extends StreamingArtists{
 	 * PROCESS: This method hopes to split the data in the LL after every 3rd comma
 	 * OUTPUT: It "hopes" to return the split up version of the LL
 	 * */
-	public static LinkedList<String> split(Scanner file) {
-		int count = 0, pos = 0; 
+	public static void printList(LinkedList<String> ordered) {
 		for(int i = 0; i < ordered.size(); i++) {
-			data = file.nextLine();
-			if (ordered.get(i).contains(",")) {
-				count++; pos++;
-				if (count == 3 && pos == 3) {
-					songs = data.split(",");
-					count = 0; pos = 0;
-				}
-			}
-			ordered = (LinkedList<String>) Arrays.asList(songs);
+			System.out.println(ordered.get(i));
 		}
-		//ordered = (LinkedList<String>) Arrays.asList(songs);
-		return ordered;
 	}
 	
 	/**INPUT: The main method always takes in a String array called "args"
@@ -112,6 +125,7 @@ public class TopStreamingArtists extends StreamingArtists{
 	 * */
 	public static void main(String[] args) throws Exception{
 		Scanner file = new Scanner(new File("names.txt"));
-		ordered = insert(file); ordered = split(file);
+		String[][] songs = StreamingArtists.readFileAndGetSongs();
+		insert(songs);
 	}
 }
